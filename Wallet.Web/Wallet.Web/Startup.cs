06a03303c195aef3b2d3781c9828188c05db1abf
Łukasz Guzik <poczta@lukaszguzik.pl>
2016-10-18 19:44:46 +0200

    Założenie projektu Wallet.Web

---
+++ Wallet.Web/Wallet.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wallet.Web.Startup))]
+namespace Wallet.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
